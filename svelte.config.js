f44cf7a210f29eaf0463f5cfbffd9ed11ba7537d
deltd3v <deltd3v@gmail.com>
2021-06-13 14:18:05 +0300

    :ok_hand:

--- svelte.config.js
+++ svelte.config.js
@@ -3,6 +3,8 @@ import preprocess from 'svelte-preprocess';
 
 import netlifyAdapter from '@sveltejs/adapter-netlify';
 
+const pkg = require('./package.json')
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
