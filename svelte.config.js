2aaa548da0e86f082abbf5f741317e5285ee2fc2
deltd3v <deltd3v@gmail.com>
2021-06-13 14:51:54 +0300

    :ok_hand:

--- svelte.config.js
+++ svelte.config.js
@@ -2,8 +2,7 @@ import preprocess from 'svelte-preprocess';
 
 import netlifyAdapter from '@sveltejs/adapter-netlify';
 
-const pkg = require('./package.json');
-
+ 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
@@ -14,13 +13,7 @@ const config = {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
 		adapter: netlifyAdapter()
-	},
-
-	vite: {
-		ssr: {
-			noExternal: Object.keys(pkg.dependencies || {})
-		}
-	}
+	} 
 };
 
 export default config;
